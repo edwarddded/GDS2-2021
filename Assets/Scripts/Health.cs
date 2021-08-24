2c23dd31edc161582aaf6a828e19c99ec1434979
Kevin Nguyen <kevin.nguyen2511@gmail.com>
2021-08-24 15:05:13 +1000

    [US:019.1] As a player I want the game to have a HealthBar to show my current Health & [US:019.2] As a player, I want the HealthBar to increase & decrease when I get hurt or restore health

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,11 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Health : MonoBehaviour
 {
+    //This health script should be reusable on all GameObjects that need health
+    //Please let me (Kevin) know if there is any issue with the script / code
+
     private int currentHealth;
     public int maxHealth;
+    public Image healthBar;
 
     // Start is called before the first frame update
     void Start()
@@ -15,13 +20,14 @@ public class Health : MonoBehaviour
 
     private void Update()
     {
+        UpdateHealthBar();
         if (currentHealth <= 0)
             Die();
     }
 
     void TakeDamage(int damage)
     {
-        // This is to prevent the player's health from going into a negative value when taking damage
+        // This is to prevent the GameObject's health from going into a negative value when taking damage
         if((currentHealth - damage) < 0)
         {
             currentHealth = 0;
@@ -30,10 +36,35 @@ public class Health : MonoBehaviour
         {
             currentHealth -= damage;
         }
+
+        UpdateHealthBar();
+    }
+
+    void Heal(int healAmount)
+    {
+        // This is to prevent the GameObject's health from going above the maximum health
+        if((currentHealth + healAmount) > maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+        else
+        {
+            currentHealth += healAmount;
+        }
+
+        UpdateHealthBar();
     }
 
     void Die()
     {
         // 
+    }
+
+    private void UpdateHealthBar()
+    {
+        // Only the player has a health bar so a null check is needed
+        // To prevent the game from breaking
+        if(healthBar != null)
+        healthBar.fillAmount = (float) currentHealth / maxHealth;
     }
 }
