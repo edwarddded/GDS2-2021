ecbcb52e3e35e098246d6edb0cfbefd0e98dac0b
edward yang <53891309+edwarddded@users.noreply.github.com>
2021-08-30 17:27:07 +1000

    US:022 As a developer, I want the game to have a Main scene so that players can press to start/Quit/Set the game
    Added Main Scene for gameplay

---
+++ Assets/Scripts/EnemyAI/SpawnEnemyAI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemyAI : MonoBehaviour
+{
+    public GameObject Enemy;
+    public int xPos;
+    public int zPos;
+    public int enemyCount;
+
+    void Start()
+    {
+        StartCoroutine(EnemyDrop());
+    }
+
+    IEnumerator EnemyDrop()
+    {
+        while (enemyCount <10)
+        {
+            xPos = Random.Range(-80, -100);
+            zPos = Random.Range(5, -5);
+            Instantiate(Enemy, new Vector3(xPos, 23, zPos), Quaternion.identity);
+            yield return new WaitForSeconds(0.1f);
+            enemyCount += 1;
+        }
+    }
+}
