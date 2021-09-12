9fb6c9d0cb68e5906963614edbb48871c215dd97
ma9yam <65475048+ma9yam@users.noreply.github.com>
2021-09-12 21:57:42 +1000

    US:040- As a player, I want to have 3 buttons UI(Map, crafting system, inventory) on the game so that the player can use a mouse to click when they don't want to press the keyboard

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ButtonManager : MonoBehaviour
+{
+    public GameObject map;
+    public GameObject build;
+    public GameObject inventory;
+
+
+  public void showMap()
+    {
+        if (map != null)
+        {
+            bool isActive = map.activeSelf;
+            map.SetActive(!isActive);
+        }
+    }
+    public void showBuild()
+    {
+        if (build != null)
+        {
+            bool isActive = build.activeSelf;
+            build.SetActive(!isActive);
+        }
+    }
+    public void showInventory()
+    {
+        if (inventory != null)
+        {
+            bool isActive = inventory.activeSelf;
+            inventory.SetActive(!isActive);
+        }
+    }
+
+
+
+}
