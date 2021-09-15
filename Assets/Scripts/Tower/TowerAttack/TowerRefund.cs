8a0d720ec6ac20093ba1a2f7d0789876a94aa0f5
Kevin Nguyen <kevin.nguyen2511@gmail.com>
2021-09-16 00:23:47 +1000

    US:019 As a developer, I want the game to have a refund system so that the player can remove the towers, and get materials back when they want to move to other places
    Added base script for refund system, needs to be integrated with towers

---
+++ Assets/Scripts/Tower/TowerAttack/TowerRefund.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerRefund : MonoBehaviour
+{
+    Inventory inventory;
+    public Item[] refundedItems;
+    public int[] itemAmounts;
+
+    bool refunded = false;
+
+    void Start()
+    {
+        inventory = Inventory.instance;
+    }
+
+    void Refund()
+    {
+        refunded = true;
+
+        for(int i = 0; i < refundedItems.Length; i++)
+        {
+            for(int j = 0; j < itemAmounts[i]; j++)
+            {
+                inventory.Add(refundedItems[i]);
+            }
+        }
+    }
+}
