d14f858affd6862cb0295e71919066407db54416
anananmianyao <56518907+anananmianyao@users.noreply.github.com>
2021-09-23 09:57:48 +1000

    US:028 As a developer, I want the game to have CutScene to show some background story at beginning of the game so that the player can understand the story.

---
+++ Assets/Scripts/Cutscene/TextMovement.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextMovement : MonoBehaviour
+{
+    public float speed;
+    void Update()
+    {
+        if (speed != 0)
+        {
+            float x = transform.localPosition.x + speed * Time.deltaTime;
+            transform.localPosition = new Vector3(x, 0, 1);
+        }
+    }
+}
