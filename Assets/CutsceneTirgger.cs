da255734c9b9b3a5f7191cc86ede9ad80fe4c234
edward yang <53891309+edwarddded@users.noreply.github.com>
2021-10-04 20:41:00 +1100

    US:49 - As a developer, I want the game to have a storyline with dialogues and cutscenes during the game playing so that it will lead players to understand the objective/goal of the game
    update the cutscene

---
+++ Assets/CutsceneTirgger.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutsceneTirgger : MonoBehaviour
+{
+    public GameObject MainCanvas;
+    public GameObject Playercamera;
+    public GameObject player, Cutscene1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            BoxCollider collider = gameObject.GetComponent<BoxCollider>();
+            collider.enabled = false;
+            StartCoroutine(Activesecne());
+        }
+    }
+
+    IEnumerator Activesecne()
+    {
+        MainCanvas.SetActive(false);
+        Playercamera.SetActive(false);
+        player.SetActive(false);
+        Cutscene1.SetActive(true);
+        yield return new WaitForSeconds(5f);
+        MainCanvas.SetActive(true);
+        Playercamera.SetActive(true);
+        player.SetActive(true);
+        Cutscene1.SetActive(false);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
