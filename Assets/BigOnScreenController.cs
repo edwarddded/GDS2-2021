e09eaebd98b923578f58a026057bd67d499c8fca
edward yang <53891309+edwarddded@users.noreply.github.com>
2021-10-22 14:44:04 +1100

    US:067 - As a player, I want the game to have the big message on the screen that it will pop up when something changes, like approaches of night, day number or the number of enemies are increased so that I can know the progress of the game and prepare before it happens

--- Assets/BigOnScreenController.cs
+++ Assets/BigOnScreenController.cs
@@ -18,118 +18,113 @@ public class BigOnScreenController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float dayCount = GameObject.Find("DayNightCycle").gameObject.GetComponent<DayNightCycle>().days;
+        
         float Time = GameObject.Find("DayNightCycle").gameObject.GetComponent<DayNightCycle>().time;
         //bool IsMorning = GameObject.Find("DayNightCycle").gameObject.GetComponent<DayNightCycle>().IsMorning;
-   
-        if (Time>0.65&&Time<0.66&& dayCount ==1)
+
+        if (Time > 0.65 && Time < 0.66)
         {
-            StartCoroutine(Day1());
+            float dayCount = GameObject.Find("DayNightCycle").gameObject.GetComponent<DayNightCycle>().days;
+            StartCoroutine(NightCount());
+            if (dayCount ==1)
+            {
+                StartCoroutine(Day1());
+
+            }
+            else if (dayCount==3)
+            {
+                StartCoroutine(Day3());
+            }
+            else if (dayCount == 6)
+            {
+                StartCoroutine(Day6());
+            }
+            else if (dayCount ==9)
+            {
+                StartCoroutine(Day9());
+            }
+            else if (dayCount ==12)
+            {
+                StartCoroutine(Day12());
+            }
+            else if (dayCount ==14)
+            {
+                StartCoroutine(Day14());
+            }
         }
-        else if (Time > 0.65 && Time < 0.66 && dayCount == 3)
-        {
-            StartCoroutine(Day3());
-        }
-        else if (Time > 0.65 && Time < 0.66 && dayCount == 6)
-        {
-            StartCoroutine(Day6());
-        }
-        else if (Time > 0.65 && Time < 0.66 && dayCount == 9)
-        {
-            StartCoroutine(Day9());
-        }
-        else if (Time > 0.65 && Time < 0.66 && dayCount == 12)
-        {
-            StartCoroutine(Day12());
-        }
-        else if (Time > 0.65 && Time < 0.66 && dayCount == 14)
-        {
-            StartCoroutine(Day14());
-        }
+    }
+    IEnumerator NightCount()
+    {
+        NightApproaches.SetActive(true);
+        float dayCount = GameObject.Find("DayNightCycle").gameObject.GetComponent<DayNightCycle>().days;
+        textMesh1.text = "NIGHT " + dayCount.ToString();
+        textMesh1.CrossFadeAlpha(1, 3, false);
+        yield return new WaitForSeconds(5f);
+        textMesh1.CrossFadeAlpha(0, 1, false);
+        NightApproaches.SetActive(false);
 
     }
     IEnumerator Day1()
     {
-        float dayCount = GameObject.Find("DayNightCycle").gameObject.GetComponent<DayNightCycle>().days;
-        //NightApproaches.SetActive(true); NightNeedtoDo.SetActive(true);
-        //yield return new WaitForSeconds(1f);
-        textMesh1.text = "NIGHT " + dayCount.ToString() + "  APPROACHES";
-        textMesh2.text = "DEFEND YOURSELF UNTIL DAY 15";
-        textMesh1.CrossFadeAlpha(1, 3, false);
+        float dayCount = GameObject.Find("DayNightCycle").gameObject.GetComponent<DayNightCycle>().days;  
+        textMesh2.text = "DEFEND YOURSELF UNTIL DAY 15";    
         textMesh2.CrossFadeAlpha(1, 2, false);     
-        yield return new WaitForSeconds(5f);
-        textMesh1.CrossFadeAlpha(0, 1, false);
+        yield return new WaitForSeconds(5f);       
         textMesh2.CrossFadeAlpha(0, 1, false);
-        NightApproaches.SetActive(false); NightNeedtoDo.SetActive(false);
+        NightNeedtoDo.SetActive(false);
 
     }
     IEnumerator Day3()
     {
         float dayCount = GameObject.Find("DayNightCycle").gameObject.GetComponent<DayNightCycle>().days;
-        NightApproaches.SetActive(true); NightNeedtoDo.SetActive(true);
-        textMesh1.CrossFadeAlpha(1, 3, false);
-        textMesh2.CrossFadeAlpha(1, 2, false);
-        textMesh1.text = "NIGHT " + dayCount.ToString() + "  APPROACHES";
+        NightNeedtoDo.SetActive(true);       
+        textMesh2.CrossFadeAlpha(1, 2, false);       
         textMesh2.text = "A HUGE WAVE OF ROBOTS IS APPROACHING TONIGHT!";
         yield return new WaitForSeconds(5f);
         textMesh1.CrossFadeAlpha(0, 1, false);
         textMesh2.CrossFadeAlpha(0, 1, false);
-        NightApproaches.SetActive(false); NightNeedtoDo.SetActive(false);
-
+        NightNeedtoDo.SetActive(false);
     }
     IEnumerator Day6()
     {
         float dayCount = GameObject.Find("DayNightCycle").gameObject.GetComponent<DayNightCycle>().days;
-        NightApproaches.SetActive(true); NightNeedtoDo.SetActive(true);
-        textMesh1.CrossFadeAlpha(1, 3, false);
-        textMesh2.CrossFadeAlpha(1, 2, false);
-        textMesh1.text = "NIGHT " + dayCount.ToString() + "  APPROACHES";
+        NightNeedtoDo.SetActive(true);       
+        textMesh2.CrossFadeAlpha(1, 2, false);        
         textMesh2.text = "THE ROBOT'S NUMBERS ARE GROWING!";
-        yield return new WaitForSeconds(5f);
-        textMesh1.CrossFadeAlpha(0, 1, false);
+        yield return new WaitForSeconds(5f);       
         textMesh2.CrossFadeAlpha(0, 1, false);
-        NightApproaches.SetActive(false); NightNeedtoDo.SetActive(false);
+        NightNeedtoDo.SetActive(false);
 
     }
     IEnumerator Day9()
     {
         float dayCount = GameObject.Find("DayNightCycle").gameObject.GetComponent<DayNightCycle>().days;
-        NightApproaches.SetActive(true); NightNeedtoDo.SetActive(true);
-        textMesh1.CrossFadeAlpha(1, 3, false);
-        textMesh2.CrossFadeAlpha(1, 2, false);
-        textMesh1.text = "NIGHT " + dayCount.ToString() + "  APPROACHES";
+         NightNeedtoDo.SetActive(true);        
+        textMesh2.CrossFadeAlpha(1, 2, false);        
         textMesh2.text = "THE MEACHANICAL WHIRRING GROWS LOUDER! SURVIVE TONIGHT!";
-        yield return new WaitForSeconds(5f);
-        textMesh1.CrossFadeAlpha(0, 1, false);
+        yield return new WaitForSeconds(5f);      
         textMesh2.CrossFadeAlpha(0, 1, false);
-        NightApproaches.SetActive(false); NightNeedtoDo.SetActive(false);
+         NightNeedtoDo.SetActive(false);
 
     }
     IEnumerator Day12()
     {
         float dayCount = GameObject.Find("DayNightCycle").gameObject.GetComponent<DayNightCycle>().days;
-        NightApproaches.SetActive(true); NightNeedtoDo.SetActive(true);
-        textMesh1.CrossFadeAlpha(1, 3, false);
-        textMesh2.CrossFadeAlpha(1, 2, false);
-        textMesh1.text = "NIGHT " + dayCount.ToString() + "  APPROACHES";
-        textMesh2.text = "THEY ARE COMING FOR YOU! COLLECT CUBE & POWEFUL TOWER";
-        yield return new WaitForSeconds(5f);
-        textMesh1.CrossFadeAlpha(0, 1, false);
+        NightNeedtoDo.SetActive(true);
+        textMesh2.CrossFadeAlpha(1, 2, false);        
+        textMesh2.text = "THEY ARE COMING FOR YOU!";
+        yield return new WaitForSeconds(5f);        
         textMesh2.CrossFadeAlpha(0, 1, false);
-        NightApproaches.SetActive(false); NightNeedtoDo.SetActive(false);
+        NightNeedtoDo.SetActive(false);
     }
     IEnumerator Day14()
     {
         float dayCount = GameObject.Find("DayNightCycle").gameObject.GetComponent<DayNightCycle>().days;
-        NightApproaches.SetActive(true); NightNeedtoDo.SetActive(true);
-        textMesh1.CrossFadeAlpha(1, 3, false);
+        NightNeedtoDo.SetActive(true);       
         textMesh2.CrossFadeAlpha(1, 2, false);
-        textMesh1.text = "NIGHT " + dayCount.ToString() + "  APPROACHES";
         textMesh2.text = "LAST NIGHT! A HUGE WAVE OF ROBOTS IS APPROACHING!";
-        yield return new WaitForSeconds(5f);
-        textMesh1.CrossFadeAlpha(0, 1, false);
+        yield return new WaitForSeconds(5f);       
         textMesh2.CrossFadeAlpha(0, 1, false);
-        NightApproaches.SetActive(false); NightNeedtoDo.SetActive(false);
-
+        NightNeedtoDo.SetActive(false);
     }
 }
